8a48fba90624057bd7a3600f8653cecca2c8b9f6
Igor <igorizr1@gmail.com>
2014-03-28 18:30:14 +0200

    mobile mock

--- www/js/app.js
+++ www/js/app.js
@@ -32,55 +32,38 @@ angular.module('ionicApp', ['ionic'])
         }
       }
     })
+    .state('eventmenu.profile-1', {
+      url: "/profile-1",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/profile-1.html"
+        }
+      }
+    })
+    .state('eventmenu.settings', {
+      url: "/settings",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/settings.html"
+        }
+      }
+    })
+    .state('eventmenu.lists', {
+      url: "/lists",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/lists.html"
+        }
+      }
+    })
   
   $urlRouterProvider.otherwise("/event/profile");
 })
 
 .controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
-  $scope.attendees = [
-    { firstname: 'Nicolas', lastname: 'Cage' },
-    { firstname: 'Jean-Claude', lastname: 'Van Damme' },
-    { firstname: 'Keanu', lastname: 'Reeves' },
-    { firstname: 'Steven', lastname: 'Seagal' }
-  ];
-  
+ 
   $scope.toggleLeft = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
 })
 
-.controller('CheckinCtrl', function($scope) {
-  $scope.showForm = true;
-  
-  $scope.shirtSizes = [
-    { text: 'Large', value: 'L' },
-    { text: 'Medium', value: 'M' },
-    { text: 'Small', value: 'S' }
-  ];
-  
-  $scope.attendee = {};
-  $scope.submit = function() {
-    if(!$scope.attendee.firstname) {
-      alert('Info required');
-      return;
-    }
-    $scope.showForm = false;
-    $scope.attendees.push($scope.attendee);
-  };
-  
-})
-
-.controller('AttendeesCtrl', function($scope) {
-  
-  $scope.activity = [];
-  $scope.arrivedChange = function(attendee) {
-    var msg = attendee.firstname + ' ' + attendee.lastname;
-    msg += (!attendee.arrived ? ' has arrived, ' : ' just left, '); 
-    msg += new Date().getMilliseconds();
-    $scope.activity.push(msg);
-    if($scope.activity.length > 3) {
-      $scope.activity.splice(0, 1);
-    }
-  };
-  
-});
